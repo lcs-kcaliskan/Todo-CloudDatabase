c24db7bdf09737fb9ac29d711131a53190d0b381
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-04-03 17:36:57 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ TodoList/Helpers/Supabase.swift
@@ -0,0 +1,15 @@
+//
+//  Supabase.swift
+//  TodoList
+//
+//  Created by Ahmet Kaan Caliskan on 2025-04-03.
+//
+
+import Foundation
+import Supabase
+ 
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://hmzhdnwhjzoumdrqwwqq.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtemhkbndoanpvdW1kcnF3d3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3MTI0MTUsImV4cCI6MjA1OTI4ODQxNX0.kT9xr9XPAFzdj8FbMkN34e14cp5ANaqp8COmavFU9Pg"
+)
+ 
